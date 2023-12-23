02d3c34d8341e5813146dc408005b3c3ebd82d04
Kyrylo Kastornov <kastornov.kir@gmail.com>
2023-12-23 20:01:59 +0200

    Initial Commit

---
+++ translator_morse/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  translator_morse
+//
+//  Created by Kyrylo Kastornov on 23.12.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
